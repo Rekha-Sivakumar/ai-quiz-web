8faaef0860f7f25fe2d0a31e595e5ab3126b4286
Rekha-Sivakumar <166686819+Rekha-Sivakumar@users.noreply.github.com>
2026-07-01 12:06:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 import os
-import google.generativeai as genai
+from google import genai
 
-# Get API key from Render Environment Variable
-api_key = os.getenv("GEMINI_API_KEY")
+API_KEY = os.getenv("GEMINI_API_KEY")
 
-# Configure Gemini
-genai.configure(api_key=api_key)
-
-# Create client/model
-client = genai.GenerativeModel("gemini-pro")
+client = genai.Client(api_key=API_KEY)
